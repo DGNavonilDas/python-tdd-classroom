f53370144bce05a5f0585dff0a66da527c6e9494
Navonil Das <navonil.das@datagrokr.com>
2021-04-12 12:04:47 +0530

    Impementations Passing all Tests

--- src/exercises.py
+++ src/exercises.py
@@ -2,10 +2,21 @@ def reverse_list(input_list):
     """
     Reverses order of elements in list input_list.
     """
-    return None
+    i = 0
+    n = len(input_list)
+    while 2*i < n:
+        # Swap i and n-i-1
+        input_list[i],input_list[n-i-1] = input_list[n - i - 1],input_list[i]
+        i += 1
+    
+    return input_list
 
 def count_digits(number):
     """
     Return count of digits
     """
-    return None
+    ans = 0
+    while number > 0:
+        ans += 1
+        number = number // 10
+    return ans
